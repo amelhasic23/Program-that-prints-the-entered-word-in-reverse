882b41b75b0f2c0cee749566f2692bf8c1dcf975
amelhasic23 <117470943+amelhasic23@users.noreply.github.com>
2024-11-28 09:09:47 +0100

    Commit

---
+++ Program koji unetu reč ispisuje obrnuto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Unesite reč: ");
+        string rec = Console.ReadLine();
+
+        char[] obrnuto = rec.ToCharArray();
+        Array.Reverse(obrnuto);
+
+        Console.WriteLine("Reč obrnuto: " + new string(obrnuto));
+    }
+}
+
---
+++ Program koji unetu reč ispisuje obrnuto/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Program koji unetu reč ispisuje obrnuto")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Program koji unetu reč ispisuje obrnuto")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0241b0fd-ee47-4019-b86c-2cb81eb9f155")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
